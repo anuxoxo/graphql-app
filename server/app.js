0bfd4f844ca86df9eddf0e854f160fa1608b46a6
Kumari Anushka <anushka.ak14@gmail.com>
2023-01-05 16:00:53 +0530

    added .env.example

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
+require("dotenv").config()
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
-const port = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
 const app = express()
+
+mongoose.connect(process.env.DB_URI)
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
 
-app.listen(port, () => {
-  console.log("Server lsitening on port " + port)
-})
+mongoose.connect(process.env.DB_URI)
+  .then(() => { app.listen(PORT, console.log("Server connected to localhost ", PORT)) })
+  .catch(err => console.log(err.message))
